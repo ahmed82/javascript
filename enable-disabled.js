2fb5e64fc6aee22d6fda4038ead0d1e80f3f46de
Ahmed Al Salih <ahmed82@users.noreply.github.com>
2020-11-11 10:49:36 -0500

    Update enable-disabled.js

--- enable-disabled.js
+++ enable-disabled.js
@@ -1,1 +1,18 @@
 
+<script> (function() {
+
+ var form = $j('#testResultsForm');
+
+form.submit(function(evt) {
+
+var disabled = form.find(':disabled');
+
+disabled.each(function() {
+
+$j(this).removeAttr('disabled');
+
+});
+
+});
+
+ })() </script>
