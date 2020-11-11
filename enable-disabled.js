5de09e13221400019776a1fa97286bbfd1a97039
Ahmed Al Salih <ahmed82@users.noreply.github.com>
2020-11-11 10:52:31 -0500

    Update enable-disabled.js

--- enable-disabled.js
+++ enable-disabled.js
@@ -4,7 +4,7 @@
  var form = $j('#testResultsForm');
 
 form.submit(function(evt) {
-
+disabled.css("pointer-events", "none"); // to disable user input 
 var disabled = form.find(':disabled');
 
 disabled.each(function() {
